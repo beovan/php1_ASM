4699c68ef4ea2262cf92324eb619960a9bf5593e
beovan <85023866+beovan@users.noreply.github.com>
2023-08-21 21:40:49 +0700

    Delete admin directory

--- admin/categories_link_products.php
+++
@@ -1,8 +0,0 @@
-<?php
- $sql_all_cate = "SELECT * FROM categories";
- $result_cate = $conn->query($sql_all_cate);
- $categories_link = [];
- while ($row = $result_cate->fetch_assoc()) {
-     $categories_link[] = $row;
- }
-?>
--- admin/layout/header.php
+++
@@ -1,9 +0,0 @@
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-  
-
-    <!-- chỉnh css mới (cart) -->
-  <link rel="stylesheet" href="../css/thongtintaikhoan.css">
-    <link rel="stylesheet" href="../css/sidebar.css">
-    <link rel="stylesheet" href="../css/dashboard.css">
